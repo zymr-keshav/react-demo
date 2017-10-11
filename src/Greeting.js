7d0138c65c80a0b6109e67a8748a11ee37af02e2
Keshav Mohta <keshav.m@keshav-m.ah.zymrinc.com>
2017-10-11 16:39:59 +0530

    remove props

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,9 +1,9 @@
 import React from 'react';
-function UserGreeting(props) {
+function UserGreeting() {
   return <h1>Welcome back!</h1>;
 }
 
-function GuestGreeting(props) {
+function GuestGreeting() {
   return <h1>Please sign up.</h1>;
 }
 
